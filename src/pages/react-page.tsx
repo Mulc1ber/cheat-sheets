9be1e7c9757c23bc23da8173bcd1542e943188e7
Mulc1ber <demushkinmo@mail.ru>
2026-01-29 10:21:34 +0300

    chore: change file "create-a-page" & create "react-page"

---
+++ src/pages/react-page.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react";
+import Layout from "@theme/Layout";
+
+export default function ReactPage(): ReactNode {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>This is a React page</p>
+    </Layout>
+  );
+}
