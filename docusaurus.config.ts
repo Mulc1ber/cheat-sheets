012a15a802fc017169cc56aee1d785a76a11bf73
Mulc1ber <demushkinmo@mail.ru>
2026-05-03 22:34:53 +0300

    feat: add env for deploy on vercel

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://mulc1ber.github.io/",
+  url: process.env.DOCUSAURUS_URL || "https://mulc1ber.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/cheat-sheets/",
+  baseUrl: process.env.DOCUSAURUS_BASE_URL || "/cheat-sheets/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
